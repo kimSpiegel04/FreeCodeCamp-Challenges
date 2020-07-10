53f08bb947ff45d5d734ddf1bf8cf81db1b57e20
kimSpiegel04 <k.spiegel72@gmail.com>
2020-07-09 21:45:27 -0400

    forgot description

--- sortedUnion.js
+++ sortedUnion.js
@@ -1,3 +1,13 @@
+// Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+// In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+// The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+// uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+// uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
+
+
 function uniteUnique(...arr) {
     var concatArr = []
     var newArr = concatArr.concat(...arr);
@@ -6,3 +16,4 @@ function uniteUnique(...arr) {
 }
 
 console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+
